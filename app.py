fea2f82fc365701b4df8574a3c9516b06c942abd
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-12-26 18:41:55 -0500

    added postgress

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, render_template
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
