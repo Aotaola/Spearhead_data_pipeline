db7b0fe11e481a418293ee223e625cfa158f719b
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-12-27 17:40:19 -0500

    added articletimespent

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 #import os
+import DateTime 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,6 +16,13 @@ class ArticleClick(db.Model):
     title = db.Column(db.String(255), unique=True, nullable=False)
     count = db.Column(db.Integer, default=0)
 
+class ArticleTimeSpent(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    article_id = db.Column(db.Integer, db.ForeignKey('article_click.id'))
+    time_spent = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, default=datetime.estnow)
+
+
 @app.route('/')
 def home():
     articles = ArticleClick.query.all()
@@ -26,21 +34,30 @@ def home():
 @app.route('/track_click', methods=['POST'])
 def track_click():
     data = request.json
+    id = data.get('id')
     title = data.get('title')
     article = ArticleClick.query.filter_by(title=title).first()
 
     if article:
         article.count += 1
     else:
-        article = ArticleClick(title=title, count = 1)
+        article = ArticleClick(id = id, title = title, count = 1)
         db.session.add(article)
     db.session.commit()
     return '',204
 
+@app.route('/track_time', methods=['POST'])
+def track_time():
+    data = request.json
+    article_id = data.get('article_id')
+    time_spent = data.get('time_spent')
 
-
+    new_time_entry = ArticleTimeSpent.query.filter_by(article_id=article_id, time_spent=time_spent)
+    db.session.add(new_time_entry)
+    db.session.commit()
 
 if __name__ == '__main__':
-    db.create_all() 
+    with app.app_context():
+        db.create_all() 
     app.run(debug=True)
 
