a306c723296c39bbf697f04243ea687da964bf8f
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-12-27 12:08:16 -0500

    integration of db

--- app.py
+++ app.py
@@ -7,25 +7,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy(app)
 
+class ArticleClick(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    count = db.Column(db.Integer, default=0)
+
 @app.route('/')
-def hello_world():
-    return render_template('root.html', click_counts = click_counts)
+def home():
+    articles = ArticleClick.query.all()
+    return render_template('root.html', articles = articles)
 
-
-click_counts = {}
+def check():
+    print ('article check', articles = ArticleClick.query.all())
 
 @app.route('/track_click', methods=['POST'])
 def track_click():
     data = request.json
     title = data.get('title')
+    article = ArticleClick.query.filter_by(title=title).first()
 
-    if title in click_counts:
-        click_counts[title] += 1
+    if article:
+        article += 1
     else:
-        click_counts[title] = 1
+        article = ArticleClick(title=title, count = 1)
+        db.session.add(article)
+    db.session.commit()
+    return 204
 
-    print(f'{title} clicked {click_counts} times')
-    return click_counts, 204
 
 
 
